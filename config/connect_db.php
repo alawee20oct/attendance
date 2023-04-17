40e4b2d270664055fed73fafd8acee551c45d6bb
Alawee Sa-i <93656325+alawee20oct@users.noreply.github.com>
2023-04-17 21:51:48 +0700

    Update connect_db.php

--- config/connect_db.php
+++ config/connect_db.php
@@ -4,10 +4,6 @@ $username = "root";
 $password = "";
 $dbname = "plan";
 
-// $servername = "localhost";
-// $username = "id20244759_rootdemo";
-// $password = "wM53u+u^+v+fgRxe";
-// $dbname = "id20244759_demo";
 $connect = mysqli_connect($servername, $username, $password, $dbname);
 if (!$connect) {
 	echo "Can't connect to Database!";
